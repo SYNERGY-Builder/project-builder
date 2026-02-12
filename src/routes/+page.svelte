4241e5b71084b257273ce781b0049840bd8c0795
diyra418@gmail.com <diyra418@gmail.com>
2026-02-12 10:32:33 +0800

    tes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,42 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import 'grapesjs/dist/css/grapes.min.css';
 
-<p>Tes</p>
+	onMount(async () => {
+		const grapesjs = (await import('grapesjs')).default;
+
+		const editor = grapesjs.init({
+			container: '#gjs',
+			fromElement: true,
+			height: '297mm',
+			width: '210mm',
+			storageManager: false
+		});
+
+		editor.BlockManager.add('section', {
+			label: `
+		<div style="padding:10px;text-align:center">
+			<strong>Section</strong>
+			<p style="font-size:12px">Layout section</p>
+		</div>
+	`,
+			category: 'Layout',
+			attributes: {
+				title: 'Insert Section'
+			},
+			content: `
+		<section style="padding:40px">
+			<h1>Put your title here</h1>
+			<div>Lorem ipsum dolor sit amet</div>
+		</section>
+	`
+		});
+	});
+</script>
+
+<div class="flex h-max items-start justify-center py-10">
+	<div id="gjs" class="w-full border-2">
+		<h1>hello world</h1>
+	</div>
+	<div id="blocks" class="w-[200px]"></div>
+</div>
