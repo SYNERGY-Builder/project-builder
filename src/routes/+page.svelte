823d8ac8b15911d09e51dfd0a6a078413f887ab9
MBARIANSYAH <edotzsilva59gmail.com>
2026-02-12 09:55:45 +0800

    Simpan file lokal sebelum pull

---
+++ src/routes/+page.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+  import { form } from '$app/server';
+    import Header from './Header.svelte';
+  
+  let formState = $state({
+     name: '',
+     birthday: '',
+     step: 0,
+     error: ''
+  });
+</script>
+
+<Header name={formState.name}>
+<p>Hello</p>
+{#snippet secondChild()}
+    <p>Second Child</p>
+{/snippet}
+
+
+</Header>
+
+<main>
+    <p>Step: {formState.step + 1}</p>
+
+{@render formStep({question: "What's your name", id: "name", type: 'text'})}
+
+    
+{#if formState.error}
+<p class="error">{formState.error}</p>
+{/if}
+
+
+
+
+
+</main>
+
+{#snippet formStep( { question, id, type}: {
+    type:string;
+    id: string; question: string
+})}
+<article>
+    <div>
+        <label for={id}>{question}</label>
+        <input {type} {id} bind:value={formState[id]}>
+    </div>
+</article>
+
+{/snippet}
+
+
+
+
+
+
+
+ 
+
---
+++ src/routes/Header.svelte
@@ -0,0 +1,14 @@
+<script>
+let {name} = $props();
+</script>
+<h1>Hello {name}</h1>
+
+<div>
+    <h1>
+        {name ? name: 'User'}'s Form'
+    </h1>
+</div>
+
+<style>
+
+</style>
